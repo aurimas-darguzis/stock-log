40d20fd441d60cfd01d9e18819b47ae257dd0d1b
aurimas-darguzis <aurimas@darguzis.com>
2019-10-06 23:15:40 +0100

    layout setup

--- src/global/components/PagesList.js
+++ src/global/components/PagesList.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import List from '@material-ui/core/ListItemText';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import GpsNotFixedIcon from '@material-ui/icons/GpsNotFixed';
+
+const Pages = ['Dashboard', 'Antibiotics'];
 
 export default function PagesList() {
-  return <div>cia bus geras listas</div>;
+  return (
+    <List>
+      {Pages.map((text, index) => (
+        <ListItem button key={index}>
+          <ListItemIcon>
+            {index === 0 ? <DashboardIcon /> : <GpsNotFixedIcon />}
+          </ListItemIcon>
+          <ListItemText primary={text} />
+        </ListItem>
+      ))}
+    </List>
+  );
 }
